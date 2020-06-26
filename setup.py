9eddc296870c5d83ced95cc8c5d2a37fdcf7b516
Rory Bentley <rbentley@astro.ucla.edu>
2020-06-26 08:18:21 -0500

    fixed setup.py so we can upload to pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name="Code/Astro NStarOrbits Project",
+    name="code-astro_nstarorbits_project",
     version="0.1",
     packages=find_packages(),
 )
